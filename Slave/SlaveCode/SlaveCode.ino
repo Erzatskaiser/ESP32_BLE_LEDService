e60fd23a8dbb2caff74d85021fbdbea496f5f154
Erzatskaiser <121765395+Erzatskaiser@users.noreply.github.com>
2024-03-04 21:57:03 -0500

    Update SlaveCode.ino

--- Slave/SlaveCode/SlaveCode.ino
+++ Slave/SlaveCode/SlaveCode.ino
@@ -1,5 +1,5 @@
 //Import needed header files
-/*#include "BLEDevice.h"
+#include "BLEDevice.h"
 
 //Define characteristics of BLE Server
 #define bleServerName "LED Service"
@@ -172,39 +172,4 @@ void loop() {
   //delay(500);
   //digitalWrite(greenPin,LOW);
   //delay(500);
-}*/
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
-
-int scanTime = 10; //In seconds
-BLEScan* pBLEScan;
-
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-    void onResult(BLEAdvertisedDevice advertisedDevice) {
-      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
-    }
-};
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Scanning...");
-
-  BLEDevice::init("");
-  pBLEScan = BLEDevice::getScan(); //create new scan
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
-  pBLEScan->setInterval(100);
-  pBLEScan->setWindow(99);  // less or equal setInterval value
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-  Serial.print("Devices found: ");
-  Serial.println(foundDevices.getCount());
-  Serial.println("Scan done!");
-  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
-  delay(2000);
 }
